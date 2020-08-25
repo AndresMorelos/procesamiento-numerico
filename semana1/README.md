08cda34af0859446d522177a405aed2fc7ec97a3
Andres <andresmorelos123@gmail.com>
2020-08-25 01:52:06 -0500

    Documentación.

--- semana1/README.md
+++ semana1/README.md
@@ -1,3 +1,9 @@
+## Solución
+Se evaluo la velocidad aproximada del segundo paracaidista en un intervalo de tiempo para determinar la cantidad de segundos que deberían transcurrir para que este alcance la velocidad del primer paracaidista en el segundo 10.
+
+## Grafica
+![Grafica](./assets/Figure_1.png)
+
 ## Requirements
 - Python 3
 - pip3 
--- semana1/init.py
+++ semana1/init.py
@@ -12,6 +12,7 @@ class CaidaLibre:
         self.imprimir_datos()
         self.velocidad_anterior = [0.0]
         self.tiempo_anterior = [0.0]
+        self.velocidad_anterior_real = [0.0]
 
     def obtener_velocidad_aproximada(self, tiempo):
         return self.velocidad_anterior[-1] + ((constantes.GRAVEDAD-((self.resistencia/self.peso) * self.velocidad_anterior[-1]))*(tiempo - self.tiempo_anterior[-1]))
@@ -19,20 +20,31 @@ class CaidaLibre:
     def obtener_velocidad(self, tiempo):
         return ((constantes.GRAVEDAD * self.peso)/self.resistencia)*(1-math.exp(-(self.resistencia/self.peso)*tiempo))
 
-    def obtener_velocidad_intervalos(self, min, max=None, interval=None, velocidad=None):
+    def obtener_resultado(self, min, max=None, interval=None, velocidad=None):
         if max == None:
             max = min
             min = 0
 
+        tiempo_aproximado = None
+        tiempo_real = None
+
         for tiempo in utils.frange(min, max, interval):
             self.velocidad_anterior.append(
                 self.obtener_velocidad_aproximada(tiempo))
+            self.velocidad_anterior_real.append(self.obtener_velocidad(tiempo))
             self.tiempo_anterior.append(tiempo)
-            if utils.isclose(self.obtener_velocidad_aproximada(tiempo), velocidad, rel_tol=0.001):
-                return tiempo
+
+            if utils.isclose(self.obtener_velocidad_aproximada(tiempo), velocidad, rel_tol=0.001) and tiempo_aproximado is None:
+                tiempo_aproximado = tiempo
+
+            if utils.isclose(self.obtener_velocidad(tiempo), velocidad, rel_tol=0.001) and tiempo_real is None:
+                tiempo_real = tiempo
+
+            if tiempo_real is not None and tiempo_aproximado is not None:
+                return [tiempo_aproximado, tiempo_real]
 
     def obtener_tiempo_aproximado(self, velocidad):
-        return self.obtener_velocidad_intervalos(min=1, max=1000, interval=0.1, velocidad=velocidad)
+        return self.obtener_resultado(min=1, max=20, interval=0.1, velocidad=velocidad)
 
     def imprimir_datos(self):
         print('|  Peso   |   Resistencia |')
@@ -40,7 +52,14 @@ class CaidaLibre:
             self.peso, self.resistencia))
 
     def mostrar_grafica(self):
-        plt.plot(self.tiempo_anterior, self.velocidad_anterior)
+        fig, ax = plt.subplots()
+        ax.plot(self.tiempo_anterior, self.velocidad_anterior, color='red')
+        ax.set_xlabel("Tiempo")
+        ax.set_ylabel("Velocidad Aproximada (roja)")
+        ax2 = ax.twinx()
+        ax2.plot(self.tiempo_anterior,
+                 self.velocidad_anterior_real, color='blue')
+        ax2.set_ylabel("Velocidad Real (azul)")
         plt.show()
 
 
@@ -49,7 +68,8 @@ objeto_uno = CaidaLibre(70, 12)
 print('Objecto Dos')
 objeto_dos = CaidaLibre(75, 15)
 
-print('tiempo aproximado {0}'.format(objeto_dos.obtener_tiempo_aproximado(
+
+print('tiempo aproximado {0}\ntiempo real {1}'.format(*objeto_dos.obtener_tiempo_aproximado(
     objeto_uno.obtener_velocidad(10))))
 
 objeto_dos.mostrar_grafica()
